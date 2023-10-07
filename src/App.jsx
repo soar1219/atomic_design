8c9622ad7b622c0fca61ec70bfddc560624f909d
soar1219 <tsubasa1219.info@gmail.com>
2023-10-08 01:09:00 +0900

    organismの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { UserCard } from "./components/organisms/user/UserCard";
 import { SearchInput } from "./components/molecules/SearchInput";
+import "./style.css"
 
+const user = {
+  name: "つばさ",
+  image: "https://source.unsplash.com/KnSYF3b4KcM",
+  email: "12345@gmail.com",
+  tel: "000-0000-0000",
+  company: {
+    name:"aaa会社"
+  },
+  website: "aaa.com",
+
+}
 
 function App() {
   return (
@@ -10,6 +23,8 @@ function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <br />
+      <UserCard user={user} />
     </div>
   );
 }
